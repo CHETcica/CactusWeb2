6be0486a593e605b1ecaedd3d0e4daba6143fdaa
CHETcica <61766789+CHETcica@users.noreply.github.com>
2021-03-04 21:17:41 +0700

    Reduce products

--- public/php/updateorder.php
+++ public/php/updateorder.php
@@ -19,6 +19,30 @@
     $stmt1 = $conn->prepare($sql1);
     $stmt1->execute();
 
+    $sql2 ="SELECT * FROM `basket` WHERE `BasketID` in ($multiBid)";
+    $stmt2 = $conn->prepare($sql2);
+    $stmt2->execute();
+   
+    while($basketupdate = $stmt2->fetch(PDO::FETCH_ASSOC)){
+        $cactusid = $basketupdate['Cac_id'];
+        $amoutcactus = $basketupdate['Amout'];
+
+
+        $cactustaget ="SELECT * FROM `product` WHERE `Cac_id`= $cactusid ";
+        $taget = $conn->prepare($cactustaget);
+        $taget->execute();
+        $cactus = $taget->fetch(PDO::FETCH_ASSOC);
+        
+        $Cac_Existing = $cactus['Cac_Existing'];
+
+        $balance = ($Cac_Existing - $amoutcactus);
+
+
+        $Updateproduct ="UPDATE `product` SET `Cac_Existing`= $balance WHERE `Cac_id` = $cactusid";
+        $stmtUpdateproduct = $conn->prepare($Updateproduct);
+        $stmtUpdateproduct->execute();
+
+    }
 
     if($stmt->execute()){
         header("location:../manageaddress.php");
